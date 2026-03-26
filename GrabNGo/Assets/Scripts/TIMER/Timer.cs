876dd3b799963c6220791e112873a5a6f2847388
danielromeuperez-commits <danielromeuperez@gmail.com>
2026-03-26 13:43:50 +0100

    ENEMIGOS AHORA, CRONOMETRO Y ITEMS LISTOS

---
+++ GrabNGo/Assets/Scripts/TIMER/Timer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TMP_Text timerText;
+
+    private float time;
+    private bool isRunning = false;
+
+    void Update()
+    {
+        if (isRunning)
+        {
+            time += Time.deltaTime;
+            UpdateTimerUI();
+        }
+    }
+
+    void UpdateTimerUI()
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        int milliseconds = Mathf.FloorToInt((time * 1000f) % 1000f);
+
+        timerText.text = $"{minutes:00}:{seconds:00}:{milliseconds:000}";
+    }
+
+    public void StartTimer()
+    {
+        isRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+}
