38993c5ec8f79cb8b989fe921d61eeb43e5e7a20
danielromeuperez-commits <danielromeuperez@gmail.com>
2026-03-31 10:10:03 +0200

    enemigo arreglado y texto

--- GrabNGo/Assets/Scripts/ITEMS/ProjectileEnemies.cs
+++ GrabNGo/Assets/Scripts/ITEMS/ProjectileEnemies.cs
@@ -2,17 +2,19 @@ using UnityEngine;
 
 public class ProjectileEnemies : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+  
 
-            if (player != null )
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+            if (player != null)
             {
                 player.DisableMovement(1); //Tiempo inmovil
             }
-            
+
             Destroy(gameObject);
 
         }
